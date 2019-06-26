c9fa8b17f3ea8a8b3b1d8e65e2f9fb9d38941bbd
DemonDaddy22 <guptarohan369@gmail.com>
2019-06-26 12:22:29 +0530

    add snackbar and relocate select box

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,18 +3,27 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import './Navbar.css';
 
 class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			format: 'hex'
+			format: 'hex',
+			open: false
 		};
 	}
 	handleChange = (e) => {
-		this.setState({ format: e.target.value });
+		this.setState({ format: e.target.value, open: true }, () => {
+			setTimeout(() => this.setState({ open: false }), 2500);
+		});
 		this.props.handleChange(e.target.value);
+	};
+	closeSnackBar = () => {
+		this.setState({ open: false });
 	};
 	render() {
 		const { format } = this.state;
@@ -36,12 +45,27 @@ class Navbar extends Component {
 					</div>
 				</div>
 				<div className="select-container">
+					{/* disable select box when snackbar is open */}
 					<Select value={format} onChange={this.handleChange}>
 						<MenuItem value="hex">HEX - #ffffff</MenuItem>
 						<MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
 						<MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
 					</Select>
 				</div>
+				<Snackbar
+					anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+					open={this.state.open}
+					autoHideDuration={2500}
+					message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
+					ContentProps={{
+						'aria-describedby': 'message-id'
+					}}
+					action={[
+						<IconButton onClick={this.closeSnackBar} color="inherit" key="close" aria-label="close">
+							<CloseIcon />
+						</IconButton>
+					]}
+				/>
 			</nav>
 		);
 	}
