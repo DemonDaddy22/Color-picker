d1cc9fbb8f4aedce2808d73ba20d4c1a85236288
DemonDaddy22 <guptarohan369@gmail.com>
2019-06-28 11:55:25 +0530

    add drawer to newPaletteForm

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import seedColours from './seedColours';
 import { generatePalette } from './colorHelper';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 class Routes extends Component {
 	getPalette = (id) => {
@@ -16,7 +17,7 @@ class Routes extends Component {
 		return (
 			<Switch>
 				<Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColours} {...routeProps} />} />
-				<Route exact path="/palette/new" render={() => <h1>New Palette Form</h1>} />
+				<Route exact path="/palette/new" render={() => <NewPaletteForm />} />
 				<Route
 					exact
 					path="/palette/:id"
