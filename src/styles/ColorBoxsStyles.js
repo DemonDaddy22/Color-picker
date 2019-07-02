4843e4f476127c69fd76b228a35f33b5a65b5848
DemonDaddy22 <guptarohan369@gmail.com>
2019-07-02 17:38:35 +0530

    add responsive helper and style colorBoxList

--- src/styles/ColorBoxsStyles.js
+++ src/styles/ColorBoxsStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js';
+import sizes from './sizes';
 export default {
 	colorBox: {
 		width: '20%',
@@ -11,6 +12,22 @@ export default {
 		'&:hover button': {
 			opacity: 1,
 			transition: 'opacity 0.5s ease-in-out'
+		},
+		[sizes.down('lg')]: {
+			width: '25%',
+			height: (props) => (props.showFullPalette ? '20%' : '50%')
+		},
+		[sizes.down('md')]: {
+			width: '50%',
+			height: (props) => (props.showFullPalette ? '10%' : '20%')
+		},
+		[sizes.down('sm')]: {
+			width: '100%',
+			height: (props) => (props.showFullPalette ? '5%' : '10%')
+		},
+		[sizes.down('xs')]: {
+			width: '100%',
+			height: (props) => (props.showFullPalette ? '5%' : '10%')
 		}
 	},
 	copyText: {
